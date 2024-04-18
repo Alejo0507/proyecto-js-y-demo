38cade0961905c3990a42b691220fae1f87c40fd
Alejo0507 <160697097+Miguis722@users.noreply.github.com>
2024-04-18 13:00:10 -0500

    cambios

---
+++ js/Bootstrap/js01.js
@@ -0,0 +1,16 @@
+console.log('hola js');
+console.error('esto es un error');
+console.info('mensaje informativo');
+console.warn('mensaje de arventencia');
+
+
+
+
+if (edad>=18){
+    console.log('es mayor de edad');
+}else{
+    console.log('menor de edad');
+}
+
+let dia= new Date().getDay();
+console.log(dia)
